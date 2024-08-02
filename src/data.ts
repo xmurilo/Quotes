b67a7e30156c2c3c17f47f8145634a1dac7478ac
Murilo <dixmurilox@gmail.com>
2024-08-02 15:36:13 -0300

    feat: Add data file with quotes
    A new data file, `data.ts`, is added to the project. This file contains an array of quotes with their respective authors. This data will be used by the Quotes component to display random quotes on the page.

---
+++ src/data.ts
@@ -0,0 +1,52 @@
+const quotes = [
+  {
+    author: "Machado de Assis",
+    text: "A vida não é mais que um sonho; mas nesse sonho não há nem riso nem prazer.",
+  },
+  {
+    author: "Clarice Lispector",
+    text: "Liberdade é pouco. O que eu desejo ainda não tem nome.",
+  },
+  {
+    author: "Carlos Drummond de Andrade",
+    text: "Ser feliz sem motivo é a mais autêntica forma de felicidade.",
+  },
+  {
+    author: "Mario Quintana",
+    text: "Eles passarão... eu passarinho!",
+  },
+  {
+    author: "Paulo Freire",
+    text: "Se a educação sozinha não transforma a sociedade, sem ela tampouco a sociedade muda.",
+  },
+  {
+    author: "Fernando Pessoa",
+    text: "O valor das coisas não está no tempo que elas duram, mas na intensidade com que acontecem. Por isso, existem momentos inesquecíveis, coisas inexplicáveis e pessoas incomparáveis.",
+  },
+  {
+    author: "Vinicius de Moraes",
+    text: "Que não seja imortal, posto que é chama, mas que seja infinito enquanto dure.",
+  },
+  {
+    author: "Monteiro Lobato",
+    text: "Um país se faz com homens e livros.",
+  },
+  {
+    author: "Guimarães Rosa",
+    text: "O real não está na saída nem na chegada: ele se dispõe para a gente é no meio da travessia.",
+  },
+  {
+    author: "Lya Luft",
+    text: "Perder-se significa ir achando e nem saber o que fazer do que for achando.",
+  },
+  {
+    author: "Adélia Prado",
+    text: "Quero cotidiano. Mudei de quereres tantas vezes, estou só esperando virar amor.",
+  },
+  {
+    author: "Cora Coralina",
+    text: "Recria tua vida, sempre, sempre. Remove pedras e planta roseiras e faz doces. Recomeça.",
+  },
+];
+
+export default quotes;
